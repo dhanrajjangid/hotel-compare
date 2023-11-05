26bf0db717b4a488873cc7a9efec16da873ad614
dhanraj jangid <dhanrajjangidst@gmail.com>
2023-11-05 23:12:15 +0530

    details on card click

--- src/components/HotelCard.jsx
+++ src/components/HotelCard.jsx
@@ -5,10 +5,12 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { useNavigate } from "react-router-dom";
 
 export default function HotelCard({ name, address, backgroundImg }) {
+  const navigate = useNavigate();
   return (
-    <Card sx={{ width: 345 }}>
+    <Card sx={{ width: 345 }} onClick={() => navigate("/details")}>
       <CardMedia
         component="img"
         alt="green iguana"
